c6913837b9f61b58c1505ce650169a360b05b531
Pattana <pattana.kaewsai@global.ntt>
2021-01-04 15:20:22 +0700

    main file

--- src/main/java/com/pattysweetapp/PattySweetMainApplication.java
+++ src/main/java/com/pattysweetapp/PattySweetMainApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PattySweetMainApplication {
-
+	/**
+	 * Run Main for this java file
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(PattySweetMainApplication.class, args);
 	}
