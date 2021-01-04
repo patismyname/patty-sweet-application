d49011866b7260bdef170a55b98f3425ed5f832c
Pattana <pattana.kaewsai@global.ntt>
2021-01-04 15:18:38 +0700

    Initial commit

---
+++ src/main/java/com/pattysweetapp/PattySweetMainApplication.java
@@ -0,0 +1,13 @@
+package com.pattysweetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PattySweetMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PattySweetMainApplication.class, args);
+	}
+
+}
